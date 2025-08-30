8da71bd1384e428a9f8f3f1cfdb90afeaff5e979
K-K-KRISHNA <kanakakrishnakeerthi@gmail.com>
2025-08-30 12:33:14 +0530

    created env files for different environments and added scripts for different environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,23 +4,28 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
+const ENV = process.env.NODE_ENV;
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_NAME'),
-        synchronize: config.get('DB_SYNC'),
-        autoLoadEntities: config.get('AUTO_LOAD'),
-      }),
+      useFactory: (config: ConfigService) => {
+        console.log('current Env: ', ENV ?? 'production');
+        return {
+          type: 'postgres',
+          host: config.get('DB_HOST'),
+          port: config.get<number>('DB_PORT'),
+          username: config.get('DB_USERNAME'),
+          password: config.get('DB_PASSWORD'),
+          database: config.get('DB_NAME'),
+          synchronize: config.get<boolean>('DB_SYNC'),
+          autoLoadEntities: config.get<boolean>('AUTO_LOAD'),
+        };
+      },
     }),
   ],
   controllers: [AppController],
