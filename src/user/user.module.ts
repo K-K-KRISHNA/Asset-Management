18d0c4828d1ce4fc1a616192d138ac34efbe4295
K-K-KRISHNA <kanakakrishnakeerthi@gmail.com>
2025-09-07 12:15:35 +0530

    modified create user method to hash the password

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,6 +7,7 @@ import { PersonalInfo } from './entities/personal-info.entity';
 import { EmploymentInfo } from './entities/employment-info.entity';
 import { Role } from 'src/roles/entities/role.entity';
 import { PaginationModule } from 'src/common/pagination/pagination.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UserController],
@@ -14,6 +15,8 @@ import { PaginationModule } from 'src/common/pagination/pagination.module';
   imports: [
     TypeOrmModule.forFeature([User, PersonalInfo, EmploymentInfo, Role]),
     PaginationModule,
+    forwardRef(() => AuthModule),
   ],
+  exports: [UserService],
 })
 export class UserModule {}
